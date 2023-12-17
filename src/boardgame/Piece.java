6fba8cc9a3246580cf3ba086d2c664611cefe4fe
Steve <falecomstevevaz@gmail.com>
2023-12-17 18:28:43 -0300

    implementing possible moves of King

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -4,7 +4,7 @@ public abstract class Piece {
 
 	protected Position position;
 	private Board board;
-
+	
 	public Piece(Board board) {
 		this.board = board;
 		position = null;
@@ -24,18 +24,6 @@ public abstract class Piece {
 		boolean[][] mat = possibleMoves();
 		for (int i=0; i<mat.length; i++) {
 			for (int j=0; j<mat.length; j++) {
-
-
-	public abstract boolean[][] possibleMoves();
-
-	public boolean possibleMove(Position position) {
-		return possibleMoves()[position.getRow()][position.getColumn()];
-	}
-
-	public boolean isThereAnyPossibleMove() {
-		boolean[][] mat = possibleMoves();
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat.length; j++) {
 				if (mat[i][j]) {
 					return true;
 				}
